ed6b56ec67dae67def32150099c0cba6a2d5a1f9
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-03-28 14:19:13 -0700

    lol

--- h7/problem2.cpp
+++ h7/problem2.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
 int main() {
+    int M, D, N;
+    double a;
+
+    cin >> M >> D >> N >> a;
+
+    vector< vector<int> > X;
+    vector<int> Y;
+    for(int j=0; j<M; j++) {
+        // dummy weights
+        X[j].push_back(1);
+        for(int i=0; i<D+1; i++) {
+            int cur;
+            cin >> cur;
+
+            if(i != D)
+                X[j].push_back(cur);
+            else
+                Y.push_back(cur);
+        }
+    }
 
     return 0;
 }

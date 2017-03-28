f513e4219aa7d4552d181af0dda6ed68b86b6162
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-03-28 00:36:40 -0700

    hey it compiles

--- h7/problem1.cpp
+++ h7/problem1.cpp
@@ -10,9 +10,14 @@ int main() {
 
     cin >> M >> D >> N >> a;
 
+    vector<double> weights;
+
     vector< vector<int> > data;
     data.resize(M);
     for(int r=0; r<M; r++) {
+        // start with all weights = 1
+        weights.push_back(1);
+
         // add dummy value
         data[r].push_back(1);
         for(int c=0; c<D+1; c++) {
@@ -22,4 +27,28 @@ int main() {
             data[r].push_back(val);
         }
     }
+
+    // main iteration loop
+    for(int n=0; n<N; n++) {
+        // get projections from current weights h(Xj)
+        vector<double> projections;
+        for(int x=0; x<M; x++) {
+            double projection = 0;
+            for(int i=0; i<D+1; i++) {
+                projection += data[x][i]*weights[i];
+            }
+
+            projections.push_back(projection);
+        }
+
+        for(int w=0; w<M; w++) {
+
+        }
+    }
+
+    // print final weights
+    for(int w=0; w<M; w++)
+        cout << fixed << showpoint << setprecision(12) << weights[w] << endl;
+
+    return 0;
 }

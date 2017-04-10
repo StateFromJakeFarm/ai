43c03cad7fb709efc9bc9a8cca77715aadb58287
Jake Hansen <jhansen44@mail.csuchico.edu>
2017-04-09 22:27:33 -0700

    documentation in .h

--- p2/ann.h
+++ p2/ann.h
@@ -13,36 +13,47 @@ class ANN {
     long double alpha;
     int k;
 
+    // Hold Neurons' input and activation function output
     struct Neuron {
         long double a;
         long double in;
     };
 
+    // Actual ANN structure composed of neurons and weights
     vector< vector<Neuron> > layers;
     vector< vector<long double> > weights;
 
+    // Digit encodings
     vector< vector<long double> > encodings;
 
+    // Train + test input/output
     vector< vector<long double> > trainIns;
     vector< vector<long double> > testIns;
     vector<long double> trainOuts;
     vector<long double> testOuts;
 
+    // Construct ANN and get input/output
     void constructLayers(char*);
     void getWeights(char*);
     void getDigitEncodings(char*);
     void getIns(char*, vector< vector<long double> >&);
     void getOuts(char*, vector<long double>&);
 
+    // Get input value for neuron
     long double getIn(int, int);
+    // Calculate activation function for neuron
     void g(int, int);
 
     void printWeights();
 
 
     public:
+    // Constructor
     ANN(char*, char*, char*, char*, char*, char*, char*, long double, int);
+    // Run one iteration of back-propagation algo
     void backPropogate();
+    // Compare output node values to digit encodings to classify the input
     void classify();
+    // Print accuracy of ANN
     void printAccuracy();
 };
